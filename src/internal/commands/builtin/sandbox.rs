fea2adcd5d9231637f030646ffd5c3fa41c338c1
Raphaël Beamonte <raphael.beamonte@gmail.com>
2025-11-07 02:12:51 -0500

    handle malformed up key

--- src/internal/commands/builtin/sandbox.rs
+++ src/internal/commands/builtin/sandbox.rs
@@ -334,7 +334,8 @@ impl SandboxCommand {
                 Some(array) => array,
                 None => {
                     // If 'up' exists but is not an array, we can't modify it
-                    return false;
+                    omni_error!("the 'up' key in .omni.yaml is not an array and cannot be modified");
+                    exit(1);
                 }
             };
 
